9411b12800f4e1d24c1ca24540567527c96fd728
Irakli Todadze <itoda20@freeuni.edu.ge>
2023-07-07 15:18:53 +0400

    init project, add dimens and colors

---
+++ app/app/src/main/java/ge/itodadze/messengerapp/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package ge.itodadze.messengerapp.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ge.itodadze.messengerapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
