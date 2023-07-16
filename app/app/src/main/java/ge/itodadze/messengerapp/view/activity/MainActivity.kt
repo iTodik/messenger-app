fe15d14349172d3df26594f51e99e546e9437078
y0kaiNearU <abeku20@freeuni.edu.ge>
2023-07-16 22:20:45 +0400

    add collapsing toolbar

--- app/app/src/main/java/ge/itodadze/messengerapp/view/activity/MainActivity.kt
+++ app/app/src/main/java/ge/itodadze/messengerapp/view/activity/MainActivity.kt
@@ -1,12 +1,32 @@
 package ge.itodadze.messengerapp.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ge.itodadze.messengerapp.R
+import ge.itodadze.messengerapp.databinding.ActivityMainBinding
+import ge.itodadze.messengerapp.databinding.ActivitySignInBinding
+import ge.itodadze.messengerapp.utils.PasswordHasher
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        registerObservers()
+        registerListeners()
+
+    }
+
+    private fun registerObservers() {
+
+    }
+
+    private fun registerListeners() {
+
     }
 }
